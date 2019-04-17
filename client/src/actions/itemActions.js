707fb412fff03cd911703e57bfcd303d9884b20b
Derick Zhang <derickzhang@fb.com>
2019-04-16 20:57:59 -0700

    Delete item action

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -5,3 +5,10 @@ export const getItems = () => {
         type: GET_ITEMS
     };
 };
+
+export const deleteItems = (id) => {
+    return {
+        type: DELETE_ITEM,
+        payload: id
+    };
+};
